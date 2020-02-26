9ece7c5734306b44748e2280728a2401fe23a42b
xzy <1640622502@qq.com>
2020-02-26 13:37:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <el-button type="primary">主要按钮</el-button>
+    <test></test>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components:{
+    test
+  }
+}
+
+import test from './components/test.vue'
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pink">
+    <h1>夏株奕喜欢{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      message: {
+        type: String,
+        default: "颜燕"
+      }
+    },
+    methods: {
+
+    },
+    data() {
+      return {
+        // message: 'yy'
+      }
+    }
+  }
+</script>
+
+<style>
+  .pink {
+    color: antiquewhite;
+    font-size: 15px;
+  }
+</style>
